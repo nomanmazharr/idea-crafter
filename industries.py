a4a7a1e74bee03d3c8bb3159bb35690f77c2d6df
Noman <nmazhar911@gmail.com>
2024-10-11 16:47:31 +0500

    initial commit

---
+++ industries.py
@@ -0,0 +1,38 @@
+industries = [
+    {
+        "name": "Sustainability",
+        "image": "images/sustainability.jpg"
+    },
+    {
+        "name": "Healthcare",
+        "image": "images/healthcare.jpg"
+    },
+    {
+        "name": "Education",
+        "image": "images/education.jpg"
+    },
+    {
+        "name": "Body and Testing",
+        "image": "images/body.jpg"
+    },
+    {
+        "name": "Smart Cities",
+        "image": "images/smart_city.jpg"
+    },
+    {
+        "name": "Entertainment",
+        "image": "images/entertainment.jpg"
+    },
+    {
+        "name": "Empowerment",
+        "image": "images/empowerment.jpg"
+    },
+    {
+        "name": "Career",
+        "image": "images/career1.jpg"
+    },
+    {
+        "name": "Technology",
+        "image": "images/technology.jpg"
+    },
+]
